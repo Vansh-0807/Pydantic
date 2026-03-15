1038e6a65082231b9ddcb770cbcf2341881a8336
Vansh-0807 <vansh.rateria15@gmail.com>
2026-03-16 02:25:01 +0530

    Made some changes

--- pydantic_01.py
+++ pydantic_01.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
     age: int
     gender: str
-    married: bool
-    allergies: List[str]
+    married: bool = False 
+    allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
 
 def insert_patient_data(patient:Patient):
@@ -25,12 +25,19 @@ def update_patient_data(patient:Patient):
 
     print('updated')
 
+# providing values 
 patient_info = {'name': 'Vansh', 'age' : 24, 'gender' : 'male',
-                'married' : False, 'allergies' : ['dust', "eye infection"], 
+                'allergies' : ['dust', "eye infection"], 
                 'contact_details' : {'email': 'vansh@gmail.com', 'phone no' : '1234567890'}}
 
+patient_info01 = {'name' : 'Harsh', 'age' : 23, 'gender': 'male',
+                'contact_details': {'email': 'harsh@gmail.com', 'phone_no': '0987654321'}}  
+
 patient1 = Patient(**patient_info)
+patient2 = Patient(**patient_info01)
 
 insert_patient_data(patient1)
+print("\n")
+insert_patient_data(patient2)
 
 
