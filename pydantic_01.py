4eb6b6d57401713c3b74b133cc7545e10b26230a
Vansh-0807 <vansh.rateria15@gmail.com>
2026-03-16 16:28:29 +0530

    Made changes

--- pydantic_01.py
+++ pydantic_01.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
+    email : EmailStr
+    linkedin_url = AnyUrl
     age: int
     gender: str
     married: bool = False 
@@ -11,6 +13,8 @@ class Patient(BaseModel):
 
 def insert_patient_data(patient:Patient):
     print("Patient Name:", patient.name)
+    print("Email ID:", patient.email)
+    print('LinkedIn Profile', patient.linkedin_url)
     print("Patient's age: ", patient.age)
     print("Gender: ", patient.gender)
     print("Married: ", patient.married)
@@ -26,12 +30,14 @@ def update_patient_data(patient:Patient):
     print('updated')
 
 # providing values 
-patient_info = {'name': 'Vansh', 'age' : 24, 'gender' : 'male',
+patient_info = {'name': 'Vansh', 'email': 'vansh@gmail.com', 
+                'linkedin_url' : 'https://linkedin.com/1322', 'age' : 24, 
+                'gender' : 'male',
                 'allergies' : ['dust', "eye infection"], 
-                'contact_details' : {'email': 'vansh@gmail.com', 'phone no' : '1234567890'}}
+                'contact_details' : {'phone no' : '1234567890'}}
 
-patient_info01 = {'name' : 'Harsh', 'age' : 23, 'gender': 'male',
-                'contact_details': {'email': 'harsh@gmail.com', 'phone_no': '0987654321'}}  
+patient_info01 = {'name' : 'Harsh', 'email' : 'harsh@gmail.com', 'age' : 23, 'gender': 'male',
+                'contact_details': {'phone_no': '0987654321'}}  
 
 patient1 = Patient(**patient_info)
 patient2 = Patient(**patient_info01)
